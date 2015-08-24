54818feac70be964f19889bda941ccbde84d98c4
farooqsaccount <farooq.account@gmail.com>
2015-08-24 09:35:54 +0000

    create chef resources and add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #This is to make the methods available in the views as well
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    @current_user ||= Chef.find(session[:chef_id]) if session[:chef_id]
+  end
+  
+  def logged_in?
+    !!current_user #This will return boolean to know whether the user is logged in or not
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform this action"
+      redirect_to :back
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,14 @@ Rails.application.routes.draw do
     end
    end
    
+   resources :chefs, except: [:new]
+   
+   get '/register', to: 'chefs#new'
+   
+   get '/login', to: 'logins#new'
+   post '/login', to: 'logins#create'
+   get '/logout', to: 'logins#destroy'
+   
    
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
